a9db098bc9a92542e875a3aae7ea25ca5241e1fa
ZuForSure <anhvux20se@gmail.com>
2025-07-10 13:09:04 +0700

    Basic Ground Movement

---
+++ Assets/_Data/Scripts/GroundMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMovement : ZuMonoBehaviour
+{
+    [SerializeField] protected float groundSpeed = 10f;
+    [SerializeField] protected bool canMove = false;
+
+    protected override void Update()
+    {
+        base.Update();
+        if (!this.canMove) return;
+        this.GroundMoving();
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    if (!this.canMove) return;
+    //    this.GroundMoving();
+    //}
+
+    protected virtual void GroundMoving()
+    {
+        transform.parent.Translate(Vector3.back * this.groundSpeed * Time.deltaTime);
+    }
+}
